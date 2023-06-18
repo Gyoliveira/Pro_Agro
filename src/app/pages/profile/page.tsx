82c97ad7d2ec4f70bef1456f810b3df13192a53e
C1330611 Markus Nathan Rufino Xavier Marques <c1330611@interno.bb.com.br>
2023-06-18 16:35:31 -0300

    input colors

--- src/app/pages/profile/page.tsx
+++ src/app/pages/profile/page.tsx
@@ -6,25 +6,24 @@ import Link from "next/link";
 import Selection from "./components/Selection";
 
 type User = {
-  id: number,
-  nome: string,
-  sobrenome: string,
-  nomeSocial: string,
-  genero: string,
-  cpf: string,
-  email: string,
-  telefone: string,
-  dataDeNascimento: string,
-  telefoneFixo: string,
-  senha: string,
-  confirmarSenha?: string
-}
+  id: number;
+  nome: string;
+  sobrenome: string;
+  nomeSocial: string;
+  genero: string;
+  cpf: string;
+  email: string;
+  telefone: string;
+  dataDeNascimento: string;
+  telefoneFixo: string;
+  senha: string;
+  confirmarSenha?: string;
+};
 
 export default function Profile() {
   const imageStyle = {
     borderRadius: "40%",
   };
-
 
   const getCepData = async (cep: string) => {
     try {
@@ -55,7 +54,6 @@ export default function Profile() {
       console.log(err);
     }
   };
-
 
   return (
     <main className="flex min-h-screen flex-row min-w bg-bg_profile bg-no-repeat bg-cover">
@@ -97,33 +95,30 @@ export default function Profile() {
         </div>
       </div>
       <div className=" flex min-w-4/5 p-5 px-20">
-        <div className="flex flex-row justify-around pt-28">
-          <div>
-            <Input placeholder="Nome" type="text" />
-            <Input placeholder="Sobrenome" type="text" />
-            <Input placeholder="Nome Social" type="text" />
-            <Input placeholder="Genero" type="text" />
-          </div>
-          <div>
-            <Input placeholder="CPF" type="text" />
-            <Input placeholder="Data de Nascimento" type="date" />
-            <Input placeholder="Celular" type="text" />
-            <Input placeholder="Telefone Fixo" type="text" />
-          </div>
-          <div>
-            <Input placeholder="Senha Atual" type="password" />
-            <Input placeholder="Nova Senha" type="password" />
-            <Input placeholder="Confirmação de Senha" type="text" />
-            <button className="text-text-light mt-10 bg-gradient-to-r from-primary-default to-primary-light w-48 py-2 rounded-full">
-              <span>Confirmar</span>
-            </button>
-          </div>
+        <div className="grid grid-cols-3 gap-10 py-16 text-text-default">
+          <Input placeholder="Nome" type="text" color="text-text-default"/>
+          <Input placeholder="Sobrenome" type="text" color="text-text-default"/>
+          <Input placeholder="Nome Social" type="text" color="text-text-default"/>
+          <Input placeholder="Genero" type="text" color="text-text-default"/>
+          <Input placeholder="CPF" type="text" color="text-text-default"/>
+          <Input placeholder="Data de Nascimento" type="date" color="text-text-default"/>
+          <Input placeholder="Celular" type="text" color="text-text-default"/>
+          <Input placeholder="Telefone Fixo" type="text" color="text-text-default"/>
+          <Input placeholder="Senha Atual" type="password" color="text-text-default"/>
+          <Input placeholder="Nova Senha" type="password" color="text-text-default"/>
+          <Input placeholder="Confirmação de Senha" type="text" color="text-text-default"/>
         </div>
-
+        
         <div className="absolute bottom-5">
-          <button onClick={() => getCepData('38413354')} className="bg-warning w-48 py-2 rounded-full text-text-light">
+          <button
+            onClick={() => getCepData("38413354")}
+            className="bg-warning w-48 py-2 rounded-full text-text-light mr-10"
+          >
             <span>Deletar Conta</span>
           </button>
+          <button className="text-text-light mt-10 bg-gradient-to-r from-primary-default to-primary-light w-48 py-2 rounded-full">
+          <span>Confirmar</span>
+        </button>
         </div>
       </div>
     </main>
